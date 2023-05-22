6abd878ee7814ce49200ca0edc748155603cc82a
Khalid Mohammad <khalid.m.sheet@gmail.com>
2023-05-22 16:13:24 +0300

    please work

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { join } from 'path';
 
 const PORT = process.env.PORT || 3000;
 
-export async function bootstrap() {
+async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors();
